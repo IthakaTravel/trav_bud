d7db5399428990cbc0ceff4ad8991862e2ebea96
vishesh <visheshbangotra@gmail.com>
2016-04-10 18:46:56 +0530

    basic apis done

---
+++ trav_bud/models/trav_bud.js
@@ -0,0 +1,235 @@
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('trav_bud', 'ironeagle', '', {
+      dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
+      port:    5432, // or 5432 (for postgres) else 3306
+    });
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+var User = sequelize.define('User', {
+    /*
+    class User:
+        name
+        nationality
+        email
+        dob
+        gender
+        bio
+        image
+    */
+    name: {
+        type: Sequelize.STRING,
+    },
+
+    nationality: {
+        type: Sequelize.STRING,
+    },
+
+    email: {
+        type: Sequelize.STRING,
+    },
+
+    dob: {
+        type: Sequelize.DATEONLY,
+    },
+
+    gender: {
+        type: Sequelize.STRING,
+    },
+
+}, {
+    freezeTableName: true,
+});
+
+
+var LocationTrip = sequelize.define('LocationTrip', {
+    /*
+    class LocationTrip:
+        start_date
+        end_date
+        location
+    */
+    start_date: {
+        type: Sequelize.DATEONLY,
+    },
+
+    end_date: {
+        type: Sequelize.DATEONLY,
+    },
+
+    location: {
+        type: Sequelize.STRING,
+    },
+
+}, {
+    freezeTableName: true
+});
+
+
+var Trip = sequelize.define('Trip', {
+    /*
+    class Trip:
+        start_date
+        end_date
+        trips: m2o -> LocationTrip
+        user: fk -> User
+    */
+    start_date: {
+        type: Sequelize.DATEONLY,
+    },
+
+    end_date: {
+        type: Sequelize.DATEONLY,
+    },
+
+    trips: {
+        type: Sequelize.STRING,
+    },
+
+}, {
+    freezeTableName: true
+});
+Trip.belongsTo(User, {'as': 'user'})
+Trip.hasMany(LocationTrip, {'as': 'location_trip'})
+
+
+var Interest = sequelize.define('Interest', {
+    /*
+    class Interest:
+        name
+    */
+    name: {
+        type: Sequelize.STRING,
+    },
+
+}, {
+    freezeTableName: true
+});
+
+
+var PreferenceOption = sequelize.define('PreferenceOption', {
+    /*
+    class PreferenceOption:
+        name
+    */
+    name: {
+        type: Sequelize.STRING,
+    },
+
+}, {
+    freezeTableName: true
+});
+
+var Preference = sequelize.define('Preference', {
+    /*
+    class Preference:
+        name
+        options: m2o -> PreferenceOption
+    */
+    name: {
+        type: Sequelize.STRING,
+    },
+    options: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+    },
+
+}, {
+    freezeTableName: true
+});
+
+
+var UserInterest = sequelize.define('UserInterest', {
+    /*
+    class UserInterest:
+        interest: fk -> Interest
+        user: fk -> User
+    */
+
+}, {
+    freezeTableName: true
+});
+UserInterest.belongsTo(User, {'as': 'user'})
+UserInterest.belongsTo(Interest, {'as': 'interest'})
+
+
+var TripInterest = sequelize.define('TripInterest', {
+    /*
+    class TripInterest:
+        preference: fk -> Interest
+        trip: fk -> Trip
+    */
+
+}, {
+    freezeTableName: true
+});
+TripInterest.belongsTo(Trip, {'as': 'trip'})
+TripInterest.belongsTo(Interest, {'as': 'interest'})
+
+
+var UserPreference = sequelize.define('UserPreference', {
+    /*
+    class UserPreference:
+        preference: fk -> Preference
+        selected options: m2o -> PreferenceOption
+        user: fk -> User
+    */
+    selected_options: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+    },
+
+}, {
+    freezeTableName: true
+});
+UserPreference.belongsTo(User, {'as': 'user'})
+UserPreference.belongsTo(Preference, {'as': 'preference'})
+
+
+var TripPreference = sequelize.define('TripPreference', {
+    /*
+    class TripPreference:
+        preference: fk -> Preference
+        selected: m2o -> PreferenceOption
+        trip: fk -> Trip
+    */
+    selected_options: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+    },
+
+}, {
+    freezeTableName: true
+});
+TripPreference.belongsTo(Trip, {'as': 'user'})
+TripPreference.belongsTo(Preference, {'as': 'preference'})
+
+
+var Match = sequelize.define('Match', {
+    /*
+    class Match:
+        requester: fk -> User
+        requestee: fk -> User
+        matched: bool
+    */
+    matched: {
+        type: Sequelize.BOOLEAN,
+    },
+
+}, {
+    freezeTableName: true
+});
+
+TripPreference.belongsTo(User, {'as': 'requester'})
+TripPreference.belongsTo(User, {'as': 'requestee'})
+
+//sequelize.sync(force=true);
+sequelize.sync();
+
+module.exports = function(sequelize, DataTypes) {
+
+    return {User: User, LocationTrip: LocationTrip, Trip: Trip, Interest: Interest, Preference: Preference, PreferenceOption: PreferenceOption, UserInterest: UserInterest, TripInterest: TripInterest, UserPreference: UserPreference, TripPreference: TripPreference, Match: Match};
+};
---
+++ trav_bud/routes/index.js
@@ -0,0 +1,258 @@
+var models  = require('../models/trav_bud.js')();
+var express = require('express');
+var router = express.Router();
+
+
+addUserDetails = (function(request_data){
+    /*
+    request_data:
+        name
+        nationality
+        email
+        dob
+        gender
+        bio
+    */
+    models.User.create({
+        name: request_data['name'],
+        nationality: request_data['nationality'],
+        email: request_data['email'],
+        gender: request_data['gender'],
+        bio: request_data['bio'],
+    }).then(function(newObj){
+        console.log(newObj);
+    });
+});
+
+addUserPreference = (function(request_data){
+    /*
+    request_data:
+        user_id: fk to User creating the trip
+        preferences:
+            [
+                id: preference id
+                options: [selected option id]
+            ]
+    */
+    request_data['preferences'] = eval(request_data['preferences'])
+    for(i in request_data['preferences']){
+        models.UserPreference.create({
+            userId: request_data['user_id'],
+            preferenceId: request_data['preferences'][i]['id'],
+            selected_options: request_data['preferences'][i]['options'],
+        }).then(function(newObj){
+            console.log('new user preference created');
+        });
+    }
+});
+
+addTripPreference = (function(request_data){
+    /*
+    request_data:
+        trip_id: fk to Trip creating the trip
+        preferences:
+            [
+                id: preference id
+                options: [selected option id]
+            ]
+    */
+    request_data['preferences'] = eval(request_data['preferences'])
+    for(i in request_data['preferences']){
+        models.TripPreference.create({
+            tripId: request_data['trip_id'],
+            preferenceId: request_data['preferences'][i]['id'],
+            selected_options: request_data['preferences'][i]['options'],
+        }).then(function(newObj){
+            console.log('new trip preference created');
+        });
+    }
+});
+
+addTrip = (function(request_data){
+    /*
+    request_data:
+        user_id: fk to User creating the trip
+    */
+    models.Trip.create({
+        UserId: request_data['user_id'],
+    }).then(function(newObj){});
+});
+
+addLocationTrip = (function(request_data){
+    /*
+    request_data:
+        trip_id: fk to trip in which location trip is being added
+        location
+        start_date
+        end_date
+    */
+    models.Trip.findAll({
+        where: {id: request_data['trip_id']}
+    }).then(function(trip){
+        models.LocationTrip.create({
+            location: request_data['location'],
+            TripId: request_data['trip_id']
+        }).then(function(locationTrip){});
+    });
+});
+
+sendAllInterests = (function(callback){
+    interests = new Array()
+    models.Interest.findAll({}).then(function(objs){
+        for(i in objs){ interests.push(objs[i]); }
+        callback(interests)
+    })
+})
+
+sendAllPreferences = (function(callback){
+    preferences = new Array()
+    models.Preference.findAll({}).then(function(objs){
+        for(i in objs){ preferences.push(objs[i]); }
+        callback(preferences)
+    })
+})
+
+
+
+// USER APIs - START //
+router.post('/api/user/add/details/', function(req, res, next) {
+    addUserDetails(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+router.get('/api/user/get/details/', function(req, res, next) {
+    models.User.find({where: {id: req.query['id']}}).then(function(user){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'user': user}));
+    });
+});
+
+router.post('/api/user/add/preference/', function(req, res, next) {
+    addUserPreference(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+router.get('/api/user/get/preference/', function(req, res, next) {
+    models.UserPreference.findAll({where: {userId: req.query['userId']}}).then(function(user_preferences){
+        console.log(user_preferences)
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'user_preferences': user_preferences}));
+    });
+});
+
+//TODO
+router.post('/api/user/add/interest/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+router.get('/api/user/get/interest/', function(req, res, next) {
+    models.UserInterest.findAll({where: {userId: req.query['userId']}}).then(function(user_interests){
+        console.log(user_interests)
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'user_interests': user_interests}));
+    });
+});
+
+//TODO
+router.post('/api/user/get/compatible/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+//TODO
+router.post('/api/user/send_request/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+//TODO
+router.post('/api/user/accept_request/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+//TODO
+router.post('/api/user/chat/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+// USER APIs - END //
+
+
+
+// TRIP APIs - START //
+router.post('/api/trip/add/', function(req, res, next) {
+    addTrip(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+router.post('/api/trip/add/location', function(req, res, next) {
+    addLocationTrip(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+router.post('/api/trip/add/preference/', function(req, res, next) {
+    addTripPreference(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+
+//TODO
+router.post('/api/trip/add/interest/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'success': true}));
+});
+// TRIP APIs - END //
+
+
+
+// INTEREST APIs - START //
+router.get('/api/interest/all/', function(req, res, next) {
+    sendAllInterests(function (interests){
+        console.log(interests)
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'interests': interests}));
+    });
+});
+
+router.get('/api/interest/', function(req, res, next) {
+    models.Interest.find({where: {id: req.query['id']}}).then(function(interest){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'interest': interest}));
+    });
+});
+// INTEREST APIs - END //
+
+
+
+// PREFERENCE APIs - START //
+router.get('/api/preference/all/', function(req, res, next) {
+    sendAllPreferences(function (preferences){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'preferences': preferences}));
+    });
+});
+
+router.get('/api/preference/', function(req, res, next) {
+    models.Preference.find({where: {id: req.query['id']}}).then(function(preference){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'preference': preference}));
+    });
+});
+
+router.get('/api/preference/option/', function(req, res, next) {
+    models.PreferenceOption.find({where: {id: req.query['id']}}).then(function(preference){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({'success': true, 'preference': preference}));
+    });
+});
+// PREFERENCE APIs - END //
+
+
+
+module.exports = router;
